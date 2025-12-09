45a7c2c23a4e93e114dbbd1862e1c57e78c95834
Matheus Costa <contato.matheusccp5@gmail.com>
2025-12-09 13:15:48 -0300

    feat: primeira estrutura do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import Heading from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
 
 export default function App() {
     return (
         <>
-            <Heading>Olá Mundo!</Heading>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti error veniam eos tempora aut assumenda officiis a consequuntur. A consequatur atque aut eligendi. Explicabo, totam rerum eius aliquid debitis id.</p>      
+            <div className="container">
+                <div className="content">
+                    <section>
+                        LOGO
+                    </section>
+                </div>
+            </div>
+            <div className="container">
+                <div className="content">
+                    <section>
+                        MENU
+                    </section>
+                </div>
+            </div>
+            <div className="container">
+                <div className="content">
+                    <section>
+                        FORM
+                    </section>
+                </div>
+            </div>
+            <div className="container">
+                <div className="content">
+                    <section>
+                        FOOTER
+                    </section>
+                </div>
+            </div>
         </>
     )
 }
