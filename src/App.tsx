d5e2ed840eff6654619a60e9c8c5ad962feacf22
Matheus Costa <contato.matheusccp5@gmail.com>
2025-12-15 20:02:22 -0300

    feat: create component DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,48 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CoutnDown } from './components/CountDown';
+import { DefaultInput } from './components/DegaultInput';
 
 export default function App() {
-    return (
-        <>
-            <Container>
-                <Logo />
-            </Container>
-            <Container>
-                <Menu />
-            </Container>
-            <Container>
-                <CoutnDown />
-            </Container>
-            <Container>
-                <Heading>FOOTER</Heading>
-            </Container>
-            
-        </>
-    )
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+      <Container>
+        <CoutnDown />
+      </Container>
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <DefaultInput
+              id='meuInput'
+              labelText='Ciclos'
+              type='text'
+              placeholder='Digite algo...'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
+    </>
+  );
 }
