2073be31739208992951402df66d265a611cba78
Matheus Costa <contato.matheusccp5@gmail.com>
2025-12-15 18:06:00 -0300

    feat: criando o component Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,24 @@
 import './styles/theme.css';
 import './styles/global.css';
 
+import { Container } from './components/Container';
+
 export default function App() {
     return (
         <>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        LOGO
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        MENU
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        FORM
-                    </section>
-                </div>
-            </div>
-            <div className="container">
-                <div className="content">
-                    <section>
-                        FOOTER
-                    </section>
-                </div>
-            </div>
+            <Container>
+                <section>LOGO</section>
+            </Container>
+            <Container>
+                <section>MENU</section>
+            </Container>
+            <Container>
+                <section>LOGO</section>
+            </Container>
+            <Container>
+                <section>FOOTER</section>
+            </Container>
+            
         </>
     )
 }
