199a24f49c5ffe218130ccbf8ca5d524a6b9cbd7
Matheus Costa <contato.matheusccp5@gmail.com>
2025-12-22 12:02:08 -0300

    feat: implementando Templates e Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,8 @@
+import { Home } from './pages/Home';
+
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CoutnDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
-
-export default function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CoutnDown />
-      </Container>
-      <Container>
-        <form action='' className='form'>
-          <div className='formRow'>
-            <DefaultInput
-              id='meuInput'
-              labelText='Ciclos'
-              type='text'
-              placeholder='Digite algo...'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-            <DefaultButton icon={<PlayCircleIcon />} color='red' />
-          </div>
-        </form>
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+export function App() {
+  return <Home />;
 }
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+export function NotFound() {
+  return(
+    <MainTemplate>
+      <Container>
+        <h1>Página não encontrada</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse explicabo consequuntur inventore. Dolorum pariatur maiores ad quia dignissimos voluptas laboriosam tempora ratione ipsa. Placeat expedita adipisci necessitatibus quos, totam eos!</p>
+      </Container>
+    </MainTemplate>
+  )
+}
